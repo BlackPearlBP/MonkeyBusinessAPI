3ba66e98b3ed391e497dbc0a46bf047c82de6054
Freegrattis <gabriel_fregatti@estudante.sesisenai.org.br>
2024-11-21 16:41:10 -0300

    :sparkles: feat: Criado novo projeto de Spring Boot
    Foi criado a estrutura e realizado as configurações iniciais.

---
+++ mkbusapi/src/main/java/com/monkeyB/mkbusapi/MkbusapiApplication.java
@@ -0,0 +1,13 @@
+package com.monkeyB.mkbusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkbusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkbusapiApplication.class, args);
+	}
+
+}
---
+++ mkbusapi/src/test/java/com/monkeyB/mkbusapi/MkbusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkeyB.mkbusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MkbusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
